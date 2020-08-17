13cc7310917084f37cbac4aafc556f16774bcd24
Kenan Kadic <ken.kadic@outlook.com>
2020-08-17 13:10:06 -0500

    initial commit

---
+++ src/main/java/org/launchcode/flagshipphonewebsite/FlagshipPhoneWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.flagshipphonewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagshipPhoneWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlagshipPhoneWebsiteApplication.class, args);
+	}
+
+}
