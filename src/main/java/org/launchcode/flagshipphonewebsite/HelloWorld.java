8320b3c6329c248978909d3da71cd63ee9a96890
Kenan Kadic <ken.kadic@outlook.com>
2020-08-19 13:42:04 -0500

    added Hello World page

---
+++ src/main/java/org/launchcode/flagshipphonewebsite/HelloWorld.java
@@ -0,0 +1,13 @@
+package org.launchcode.flagshipphonewebsite;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+}
