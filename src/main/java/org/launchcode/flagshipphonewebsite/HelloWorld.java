fe4bb9cc724a48589396dc3cb2f37d6fdadb829d
Kenan Kadic <ken.kadic@outlook.com>
2020-08-20 20:07:42 -0500

    Hello World Commented out and MySql added to application.properties

--- src/main/java/org/launchcode/flagshipphonewebsite/HelloWorld.java
+++ src/main/java/org/launchcode/flagshipphonewebsite/HelloWorld.java
@@ -1,13 +1,13 @@
-package org.launchcode.flagshipphonewebsite;
+//package org.launchcode.flagshipphonewebsite;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloWorld {
+//@RestController
+//public class HelloWorld {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello World";
-    }
-}
+//    @RequestMapping("/")
+//    public String index() {
+//        return "Hello World";
+//    }
+//}
